88c63ce872d1deeb1d5daecbe646459d1bfaaf5b
Salman594824 <montclaire91@gmail.com>
2026-02-10 12:10:10 +0500

    Updated Footer.tsx to add social media links

---
+++ Footer.tsx
@@ -0,0 +1,16 @@
+// Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <div className="social-media">
+                <a href="https://instagram.com/montclaire01">Instagram</a>
+                <a href="https://pinterest.com/montclaire91">Pinterest</a>
+                <a href="https://tiktok.com/@montclaire001">TikTok</a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
